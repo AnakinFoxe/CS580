e3e78c7af638e73ab70de46acfa97b66c7f9b483
AnakinFoxe <xing.y.hu@gmail.com>
2014-03-04 21:54:17 -0800

    Merged Lupe's update of Administrator Panel

--- MeetingScheduler/src/scheduler/gui/AdministratorPanel.java
+++ MeetingScheduler/src/scheduler/gui/AdministratorPanel.java
@@ -1,5 +1,8 @@
 package scheduler.gui;
 
+import java.awt.CardLayout;
+import java.awt.Color;
+
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 
@@ -14,75 +17,84 @@ public class AdministratorPanel extends JPanel {
 	private JLabel lblRooms;
 	private JScrollPane emScrollPane;
 	private JScrollPane roomScrollPane;
-	private JButton btnAdd;
-	private JButton btnModify;
-	private JButton btnDelete;
+	private JButton btnAddEmp;
+	private JButton btnModifyEmp;
+	private JButton btnAddRoom;
+	private JButton btnModifyRoom;
+	private JButton btnDeleteRoom;
+	private JButton btnDeleteEmp;
 	private JList emList;
 	private JList roomList;
-	private GroupLayout groupLayout;
+	private CardLayout cardlayout;
+	private JPanel controller;
 	
 	public AdministratorPanel() {
 		lblEmployees = new JLabel("Employees");
 		lblRooms = new JLabel("Rooms");
 		emScrollPane = new JScrollPane();
 		roomScrollPane = new JScrollPane();
-		btnAdd = new JButton("Add");
-		btnModify = new JButton("Modify");
-		btnDelete = new JButton("Delete");
+		btnAddEmp = new JButton("Add Employee");
+		btnModifyEmp = new JButton("Modify Employee");
+		btnAddRoom = new JButton("Add Room");
+		btnModifyRoom = new JButton("Modify Room");
+		btnDeleteRoom = new JButton("Delete Room");
+		btnDeleteEmp = new JButton("Delete Employee");
 		emList = new JList();
 		roomList = new JList();
-		groupLayout = new GroupLayout(this);
 		
-		SetUpPanel();
-	}
-	private void SetUpPanel(){
-
+		
 		emScrollPane.setViewportView(emList);
-		roomScrollPane.setRowHeaderView(roomList);
-		this.setLayout(groupLayout);
-		groupLayout.setHorizontalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addGap(18)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(lblEmployees, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
-								.addGap(91)
-								.addComponent(lblRooms))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(emScrollPane, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addComponent(roomScrollPane, GroupLayout.PREFERRED_SIZE, 144, GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(btnAdd, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-									.addComponent(btnModify)
-									.addComponent(btnDelete, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE))))
-						.addGap(41))
-			);
-			groupLayout.setVerticalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(11)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(lblEmployees, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-									.addGroup(groupLayout.createSequentialGroup()
-										.addGap(2)
-										.addComponent(lblRooms, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)))
-								.addGap(6)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(emScrollPane)
-									.addComponent(roomScrollPane, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(67)
-								.addComponent(btnAdd)
-								.addGap(6)
-								.addComponent(btnModify)
-								.addGap(6)
-								.addComponent(btnDelete)))
-						.addContainerGap(26, Short.MAX_VALUE))
-			);
+		roomScrollPane.setViewportView(roomList);
+		
+		add(lblEmployees);
+		add(lblRooms);
+		add(emScrollPane);
+		add(roomScrollPane);
+		add(btnAddRoom);
+		add(btnModifyRoom);
+		add(btnDeleteRoom);
+		add(btnAddEmp);
+		add(btnModifyEmp);
+		add(btnDeleteEmp);
+		
+		setBackground(Color.LIGHT_GRAY);
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.NORTH, btnAddEmp, 231, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.NORTH, roomScrollPane, 6, SpringLayout.SOUTH, lblRooms);
+		springLayout.putConstraint(SpringLayout.SOUTH, roomScrollPane, -6, SpringLayout.NORTH, btnAddRoom);
+		springLayout.putConstraint(SpringLayout.WEST, btnAddRoom, 106, SpringLayout.EAST, btnAddEmp);
+		springLayout.putConstraint(SpringLayout.EAST, btnAddRoom, 0, SpringLayout.EAST, btnModifyRoom);
+		springLayout.putConstraint(SpringLayout.NORTH, emScrollPane, 6, SpringLayout.SOUTH, lblEmployees);
+		springLayout.putConstraint(SpringLayout.SOUTH, emScrollPane, -6, SpringLayout.NORTH, btnAddEmp);
+		springLayout.putConstraint(SpringLayout.EAST, btnAddEmp, 0, SpringLayout.EAST, btnModifyEmp);
+		springLayout.putConstraint(SpringLayout.WEST, roomScrollPane, 76, SpringLayout.EAST, emScrollPane);
+		springLayout.putConstraint(SpringLayout.EAST, roomScrollPane, -65, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.EAST, emScrollPane, 166, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.WEST, lblRooms, 168, SpringLayout.EAST, lblEmployees);
+		springLayout.putConstraint(SpringLayout.WEST, lblEmployees, 23, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnDeleteRoom, 106, SpringLayout.EAST, btnDeleteEmp);
+		springLayout.putConstraint(SpringLayout.WEST, btnModifyRoom, 106, SpringLayout.EAST, btnModifyEmp);
+		springLayout.putConstraint(SpringLayout.NORTH, lblRooms, 0, SpringLayout.NORTH, lblEmployees);
+		springLayout.putConstraint(SpringLayout.WEST, btnDeleteEmp, 23, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnModifyEmp, 23, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnAddEmp, 23, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, lblEmployees, 10, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnDeleteEmp, 0, SpringLayout.NORTH, btnDeleteRoom);
+		springLayout.putConstraint(SpringLayout.NORTH, btnModifyEmp, 0, SpringLayout.NORTH, btnModifyRoom);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnModifyRoom, -65, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnDeleteRoom, 15, SpringLayout.SOUTH, btnModifyRoom);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnAddRoom, -16, SpringLayout.NORTH, btnModifyRoom);
+		springLayout.putConstraint(SpringLayout.WEST, emScrollPane, 23, SpringLayout.WEST, this);
+		setLayout(springLayout);
+		
+		
+	
+		
+	}
+	
+	protected void getData() {
+		// TODO Auto-generated method stub
+		controller = (JPanel) this.getParent();
+		cardlayout  = (CardLayout) controller.getLayout();
 	}
 }
