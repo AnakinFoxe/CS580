9dfbd6f57769407e2fc45fc9aa8897037fca1b74
Brian <brntruong@gmail.com>
2014-03-19 04:35:47 -0700

    The modify meeting now modifies

--- MeetingScheduler/src/scheduler/model/RoomModel.java
+++ MeetingScheduler/src/scheduler/model/RoomModel.java
@@ -1,4 +1,4 @@
-<<<<<<< Upstream, based on master/master
+
 package scheduler.model;
 
 import java.beans.PropertyChangeListener;
@@ -31,37 +31,4 @@ public class RoomModel {
 	
 	
 }
-=======
-package scheduler.model;
 
-import java.beans.PropertyChangeListener;
-
-import javax.swing.event.SwingPropertyChangeSupport;
-
-public class RoomModel {
-	private SwingPropertyChangeSupport pcSupport = new SwingPropertyChangeSupport(this);
-	public static final String modelName = "ROOM";
-	private Room room;
-	
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		pcSupport.addPropertyChangeListener(listener);
-	}
-
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		pcSupport.removePropertyChangeListener(listener);
-	}
-
-	public Room getRoom() {
-		return room;
-	}
-
-	public void setRoom(Room room) {
-		Room oldRoom = this.room;
-		this.room = room;
-		Room newRoom = this.room;
-		pcSupport.firePropertyChange(modelName, oldRoom, newRoom);
-	}
-	
-	
-}
->>>>>>> e4d4fdb added fixed for ppanel to update
