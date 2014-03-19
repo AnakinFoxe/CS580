36de31c0aa6ab565c9e2e0ddd517bda4f45aa8b1
Brian <brntruong@gmail.com>
2014-03-19 03:03:06 -0700

    added more models to work with modify meeting

---
+++ MeetingScheduler/src/scheduler/gui/RoomModel.java
@@ -0,0 +1,33 @@
+package scheduler.gui;
+
+import java.beans.PropertyChangeListener;
+import javax.swing.event.SwingPropertyChangeSupport;
+
+import scheduler.model.Room;
+
+public class RoomModel {
+	private SwingPropertyChangeSupport pcSupport = new SwingPropertyChangeSupport(this);
+	public static final String modelName = "ROOM";
+	private Room room;
+	
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		pcSupport.addPropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		pcSupport.removePropertyChangeListener(listener);
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		Room oldRoom = this.room;
+		this.room = room;
+		Room newRoom = this.room;
+		pcSupport.firePropertyChange(modelName, oldRoom, newRoom);
+	}
+	
+	
+}
--- MeetingScheduler/src/scheduler/gui/RoomPanel.java
+++ MeetingScheduler/src/scheduler/gui/RoomPanel.java
@@ -17,12 +17,14 @@ import scheduler.model.DateModel;
 import scheduler.model.Employee;
 import scheduler.model.EmployeeListModel;
 import scheduler.model.EmployeeModel;
+import scheduler.model.Flag;
 import scheduler.model.Room;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.Date;
 import java.util.List;
 
@@ -43,35 +45,41 @@ public class RoomPanel extends JPanel {
 	private ButtonGroup roomGroup;
 	private List<Employee> attendees;
 	private EmployeeModel user;
-	
+	private Flag isHomeVisible;
+	private Flag rPanelVisible;
+	private Flag fromMeetingDet;
+	private Room selectedRoom;
+
 	public RoomPanel() {
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
-		
+
 		roomBox = Box.createVerticalBox();
 		scrollPane = new JScrollPane(roomBox);
 		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 68, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 41, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, scrollPane, 194, SpringLayout.WEST, this);
 		add(scrollPane);
-		
+
 		roomLabel = new JLabel("Choose a Room");
 		springLayout.putConstraint(SpringLayout.NORTH, roomLabel, 32, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, roomLabel, 199, SpringLayout.WEST, this);
 		add(roomLabel);
-		
+
 		finishBtn = new JButton("finish");
 		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, 0, SpringLayout.SOUTH, finishBtn);
 		finishBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (controller == null){
-            		getData();
-            	}
+					getData();
+				}
 				boolean roomSelected = getRoom();
 				if(roomSelected){
+					isHomeVisible.setFlag(true);
+					rPanelVisible.setFlag(false);
 					cardlayout.show(controller,"home");
 				}
-            	
+
 			}
 
 		});
@@ -81,20 +89,20 @@ public class RoomPanel extends JPanel {
 		springLayout.putConstraint(SpringLayout.EAST, finishBtn, -26, SpringLayout.EAST, this);
 		add(finishBtn);
 	}
-	
+
 	protected boolean getRoom() {
 		// TODO Auto-generated method stub
 		boolean isSelected = false;
 		for(int i = 0; i < roomBox.getComponentCount(); i++){
 			JRadioButton rdbtn = (JRadioButton) roomBox.getComponent(i);
 			if(rdbtn.isSelected()){
-//				Room selectedRoom = availableRooms.get(i);
-//				List<Employee> attendees = attendeeList.getList();
-//				Date selectedDate = this.selectedDate.getDate();
+				//				Room selectedRoom = availableRooms.get(i);
+				//				List<Employee> attendees = attendeeList.getList();
+				//				Date selectedDate = this.selectedDate.getDate();
 				Controller.insertMeeting(user.getEmployee(),
-										attendeeList.getList(),
-										this.selectedDate.getDate(),
-										availableRooms.get(i));
+						attendeeList.getList(),
+						this.selectedDate.getDate(),
+						availableRooms.get(i));
 				isSelected = true;
 			}
 		}
@@ -106,12 +114,12 @@ public class RoomPanel extends JPanel {
 		controller = (JPanel) this.getParent();
 		cardlayout  = (CardLayout) controller.getLayout();
 	}
-	
+
 	public void setModel(EmployeeListModel employeeList) {
 		// TODO Auto-generated method stub
 		this.attendeeList = employeeList;
 		/*attendeeList.addPropertyChangeListener(new PropertyChangeListener() {
-			
+
 			public void propertyChange(PropertyChangeEvent evt) {
 				// TODO Auto-generated method stub
 				if(EmployeeListModel.modelName.equals(evt.getPropertyName())){
@@ -120,18 +128,27 @@ public class RoomPanel extends JPanel {
 			}
 		});*/
 	}
-	
+
 	public void setModel(EmployeeModel employee){
 		this.user = employee;
 	}
 	public void setModel(DateModel dateModel){
 		this.selectedDate = dateModel;
-		selectedDate.addPropertyChangeListener(new PropertyChangeListener() {
-			
-			public void propertyChange(PropertyChangeEvent arg0) {
+	}
+
+	public void setModel(Flag flag){
+		this.isHomeVisible = flag;
+	}
+
+	public void setFlag(Flag roomPVisible) {
+		this.rPanelVisible = roomPVisible;
+
+		rPanelVisible.addPropertyChangeListener(new PropertyChangeListener() {
+
+			public void propertyChange(PropertyChangeEvent evt) {
 				// TODO Auto-generated method stub
-				if(DateModel.modelName.equals(arg0.getPropertyName())){
-					//add method for date
+				boolean isVisible = rPanelVisible.getFlag();
+				if(isVisible){
 					Date selected = selectedDate.getDate();
 					attendees = attendeeList.getList();
 					roomBox.removeAll();
@@ -139,11 +156,24 @@ public class RoomPanel extends JPanel {
 					availableRooms = Controller.genRoomList(selected, attendees.size());
 					for(int i = 0; i < availableRooms.size(); i++){
 						JRadioButton rdbtn = new JRadioButton(availableRooms.get(i).getName().toString()); 
+						if(selectedRoom != null && fromMeetingDet.getFlag()){
+							rdbtn.setSelected(true);
+						}
 						roomBox.add(rdbtn);
 						roomGroup.add(rdbtn);
 					}
 				}
 			}
 		});
+	}
+
+	public void setDFlag(Flag fromMeetingDet) {
+		// TODO Auto-generated method stub
+		this.fromMeetingDet = fromMeetingDet;
+	}
+	
+	public void setModel(Room model) {
+		// TODO Auto-generated method stub
+		this.selectedRoom = model;
 	}
 }
